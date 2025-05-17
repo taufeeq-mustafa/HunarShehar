42a8b9add51013492e7b941f9b602cb5c975563e
taufeeq <158837644+taufeeq-mustafa@users.noreply.github.com>
2025-05-17 12:50:47 +0500

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AgentProvider } from './context/AgentContext';
 import Login from './components/Login/Login';
+import Register from './components/Login/register.jsx';
 import Dashboard from './components/Dashboard/Dashboard';
 import DomainSelection from './components/DomainSelection/DomainSelection';
 import LearningModule from './components/LearningModule/LearningModule';
@@ -15,6 +16,8 @@ function App() {
         <AgentProvider>
           <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route
               path="/dashboard"
               element={
